05a31ecb46bb3044a7fd051c85ea32c43ac01b8b
Note <chl8263@naver.com>
2018-03-30 00:07:22 +0900

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String []args){
+
+        while (true){
+            Scanner scanner = new Scanner(System.in);
+            scanner.next();
+            try {
+                MqttClient client = new MqttClient("tcp://192.168.219.111:1883",MqttClient.generateClientId());
+                client.connect();
+                client.publish("aa","helo".getBytes(),0,false);
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
